ac21e8734650b1f75c97f7acfb2435ffc21913b9
nower87 <152420431+nower87@users.noreply.github.com>
2024-03-21 12:05:09 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+    public static void main(String[] args) {
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+public class RandomDataGenerator {
+    }
+        int[] data = new int[10];
+        }
+        }
+}
+        for (int i = 0; i < 10; i++) {
